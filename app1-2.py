1c53ba7496c66c07992f52c85af3d4c217cb223e
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-08-17 05:22:21 +0900

    Update app1-2.py

--- app1-2.py
+++ app1-2.py
@@ -17,6 +17,7 @@ st.title('mountain')
 m = folium.Map(location=[36.736429,127.801687], zoom_start=7)#기본지도
 m.add_child(folium.LatLngPopup())#클릭시 위도경도 표시
 app = Nominatim(user_agent='tutorial')#고유명칭으로 주소를 가져온다
+print(m)
         
         
 
